d33a0b8b16ce03f0d71fc3e0bdad620d4cb11f3a
Marin <marin.fayolle@gmail.com>
2020-02-21 13:32:16 +0100

    Login functional

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -16,21 +16,14 @@ class AdminController
 
     public function showAdminPanel($name, $password)
     {
-        if ($_POST['admin_name'] === $name || $_POST['password'] === $password) {
-            $adminManager = new AdminManager();
-            if (!$adminManager->getAuth($name, $password)) {
-                echo 'nope';
-                $this->msg = 'Invalid';
-                require 'view/adminLoginView.php';
-                return;
-            } else {
-                var_dump($name, $password);
-                echo 'coucou';
-                $this->msg = 'Admin';
-                require 'view/adminView.php';
-                return;
-            }
+        var_dump($name, $password);
+        $adminManager = new AdminManager();
+        if (!$adminManager->getAuth($name, $password)) {
+            echo 'nope';
+            $this->msg = 'Invalid';
+            require 'view/adminLoginView.php';
         } else {
+            var_dump($name, $password);
             echo 'coucou';
             $this->msg = 'Admin';
             require 'view/adminView.php';
